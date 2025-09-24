0ee9f2f5ca6113cbc37964fb4fe4e0a644c54867
Dominick <dominickfrench.oe@gmail.com>
2025-09-24 11:49:45 -0500

    initial commit

---
+++ src/main/java/com/pluralsight/Receipt.java
@@ -0,0 +1,14 @@
+package com.pluralsight;
+
+public class Receipt {
+    public static void main(String[] args) {
+
+        String itemname = "Apples";
+        int itemamount = 6;
+        double itemprice = 1.25;
+
+        System.out.println("You bought " + itemamount + " " + itemname + " for $7.50");
+
+
+    }
+}
