f7f79dd28f85a3a8af979462fcaf085b27cbd13e
Dominick <dominickfrench.oe@gmail.com>
2025-09-24 11:13:19 -0500

    Commit again

---
+++ src/main/java/com/pluralsight/Receipt.java
@@ -0,0 +1,29 @@
+package com.pluralsight;
+
+public class Receipt {
+    public static void main(String[] args) {
+        // Declare and initialize variables
+
+        //Days in a week
+        int daysinweek = 7;
+
+        System.out.println("There are " + daysinweek + " days in a week.");
+
+        //The price of coffee ($4.99)
+        double coffeePrice = 4.99;
+
+        System.out.println("The price of coffee is $" + coffeePrice + ".");
+
+        //Your favorite letter
+        char favoriteLetter = 'D';
+
+        System.out.println("My favorite letter is '" + favoriteLetter + "'.");
+
+        //Whether it's raining (true/false)
+        boolean isRaining = true;
+
+        System.out.println("Is it raining today? " + isRaining);
+
+    }
+}
+
