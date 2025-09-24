6d6ef6a4aa477e9f82b9693d83aaef73033e6bca
Dominick <dominickfrench.oe@gmail.com>
2025-09-24 10:54:09 -0500

    Commit again

---
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -0,0 +1,44 @@
+package com.pluralsight;
+
+public class VariableApp {
+    public static void main(String[] args) {
+        /** Question 1: Declare a variable to store your favorite color
+         * Declaring variable is a String
+         */
+        String favoritecolor = "Blue";
+
+        System.out.println("The answer is " + favoritecolor);
+
+        /** Question 2: Declare a variable to store the year you started this class
+         * Declaring variable is int
+         */
+        int startingyear = 2025;
+
+        System.out.println("The answer is " + startingyear);
+
+        /** Question 3: Declare a variable to store your middle initial
+         * Declaring variable is char
+         */
+        char middleinitial = 'n';
+
+        System.out.println("The Answer is " + middleinitial);
+
+        /** Question 4: Declare a variable to store whether you have pets (yes/no)
+         * Declaring variable is boolean
+         */
+        boolean havepets = true;
+
+        System.out.println("The Answer is " + havepets);
+
+        /** Question 5: Declare a variable with a nice message or saying
+         * Declaring variable is string
+         */
+        String nicemessage = "Dave's jokes help my goofy brain wake up in the morning.";
+
+        System.out.println("The Answer is " + nicemessage);
+
+
+    }
+
+
+}
